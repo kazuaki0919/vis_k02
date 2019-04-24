31547caf89f3a7cd57b82d124d460d66d4e01879
並木一晃 <developer.ios.kazuaki@gmail.com>
2019-04-24 12:00:31 +0900

    ビジュアライゼーション第2回課題

--- k02/k02.c
+++ k02/k02.c
@@ -12,8 +12,9 @@
 #include "myShape.h"
 #define KEY_ESC 27
 
-float dist=0.0;  //視点の移動量
-float theta =0.0;
+float dist = -20.0;  //視点の移動量
+float theta = 0.0;  //ロケット回転
+float speed = 0.0;  //打ち上げスピード
 
 void display(void)
 {
@@ -21,78 +22,75 @@ void display(void)
     
     glClear(GL_COLOR_BUFFER_BIT);
     
-    glPushMatrix();
-    gluLookAt(0.0,1.0,dist,0.0,1.0,dist+1.0,0.0,1.0,0.0);
-//    glTranslatef(0.0,0.0,-20.0);
-//    glRotatef(theta,0.0,10.0,0.0);
+    glPushMatrix();  //8:全体
+    gluLookAt(0.0, 1.0, dist, 0.0, 1.0, dist + 1.0, 0.0, 1.0, 0.0);
+    
+    glPushMatrix();  //6:ロケット
+    glTranslatef(0.0, speed, 0.0);
+    glRotatef(theta, 0.0, 10.0, 0.0);
     
     //三角錐テッペン
-    glPushMatrix();
-    glTranslatef(0.0,2.0,10.0);
-    glRotatef(-90.0,1.0,0.0,0.0);
-    glutWireCone(0.5,1.0,12,3);
-    glPopMatrix();
+    glPushMatrix();  //1;三角錐テッペン
+    glTranslatef(0.0, 2.0, 0.0);
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    glutWireCone(1.0, 2.0, 12.0, 3.0);
+    glPopMatrix();  //1:三角錐テッペン
     
     //円柱上
-    glPushMatrix();
-    glColor3f(1.0,0.0,0.0);
-    glTranslatef(0.0,1.5,10.0);
-    myWireCylinder(0.5,1.0,12);
-    glPopMatrix();
+    glPushMatrix();  //2:円柱上
+    glTranslatef(0.0, 1.0, 0.0);
+    myWireCylinder(1.0, 2.0, 12.0);
+    glPopMatrix();  //2:円柱上
     
     //円柱下
-    glPushMatrix();
-    glColor3f(0.0,1.0,0.0);
-    glTranslatef(0.0,0.5,10.0);
-    myWireCylinder(0.5,1.0,12);
-    glPopMatrix();
+    glPushMatrix();  //3:円柱下
+    glTranslatef(0.0, -1.0, 0.0);
+    myWireCylinder(1.0, 2.0, 12.0);
+    glPopMatrix();  //3:円柱下
     
     //三角錐噴射口
-    glPushMatrix();
-    glColor3f(0.0,0.0,1.0);
-    glTranslatef(0.0,-0.25,10.0);
-    glRotatef(-90.0,1.0,0.0,0.0);
-    glutWireCone(0.25,0.5,12,3);
-    glPopMatrix();
+    glPushMatrix();  //4:三角錐噴射口
+    glTranslatef(0.0, -2.5, 0.0);
+    glRotatef(-90.0, 1.0, 0.0, 0.0);
+    glutWireCone(0.5, 1.0, 12.0, 3.0);
+    glPopMatrix();  //4:三角州噴射口
     
     //羽根生成
     for (i = 0; i < 4; i++) {
-//        glRotatef(90.,0.,1.,0.);
-//        glColor3f(1.0,1.0,0.0);
-//        glPushMatrix();
-//        glTranslatef(1.5,-1.0,10.0);
-//        glScalef(1.0, 2.0, 0.2);
-//        glutWireCube(1.0);
-//        glPopMatrix();
-        
-        glRotatef(90.,0.,1.,0.);
-        glColor3f(1.0,1.0,0.0);
-        glPushMatrix();
-        glTranslatef(0.0,1.0,0.0);
-        glScalef(0.2, 0.2, 0.2);
+        glRotatef(90.0, 0.0, 1.0, 0.0);
+        glPushMatrix();  //5:羽根
+        glTranslatef(1.5, -1.0, 0.0);
+        glScalef(1.0, 2.0, 0.2);
         glutWireCube(1.0);
-        glPopMatrix();
+        glPopMatrix();  //5:羽根
     }
+    
+    glPopMatrix();  //6:ロケット
+    glFlush();
+    
+    glPushMatrix();  //7:地面
     
     //地面
-    glColor3f(1.0,1.0,1.0);
+    glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_LINES);
-    for( i= -35;i<36;i+=2){
-        glVertex3f((float)i,0,-35.0);
-        glVertex3f((float)i,0,35.0);
-        glVertex3f(-50.0,0,(float)i);
-        glVertex3f(50.0,0,(float)i);
+    for(i = -35; i < 36; i += 2){
+        glVertex3f((float)i, 0.0, -35.0);
+        glVertex3f((float)i, 0.0, 35.0);
+        glVertex3f(-50.0, 0.0, (float)i);
+        glVertex3f(50.0, 0.0, (float)i);
     }
     glEnd();
+    glPopMatrix(); //7:地面
     
-    glPopMatrix();
+    glPopMatrix(); //8:全体
     glFlush();
 }
 
 void idle(void)
 {
     printf("theta = %f\n", theta);
-    theta=fmod(theta+0.1,360.0);
+    theta = fmod(theta + 0.1, 360.0);
+    speed += 0.05;  //自分のMacBook Proの場合は、0.05, 学内PCの場合は、0.001で動かす
     glutPostRedisplay();  //ディスプレイコールバックバック関数(display)を実行
 }
 
@@ -102,18 +100,18 @@ void myKbd(unsigned char key, int x, int y)
 }
 void myInit(char *progname)
 {
-    int width=500,height=500;
-    float aspect=(float)width/(float)height;
+    int width = 500,height = 500;
+    float aspect = (float)width / (float)height;
     
-    glutInitWindowPosition(0,0);
-    glutInitWindowSize(width,height);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(width, height);
     glutInitDisplayMode(GLUT_RGBA);
     glutCreateWindow(progname);
-    glClearColor(0.0,0.0,0.0,1.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glutKeyboardFunc(myKbd);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60.0,aspect,0.1,40.0);
+    gluPerspective(60.0, aspect, 0.1, 40.0);
     glMatrixMode(GL_MODELVIEW);
 }
 int main(int argc, char *argv[])
